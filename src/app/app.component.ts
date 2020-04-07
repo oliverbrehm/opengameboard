1cdce95a619449a00e880634a6826c062ddd0975
Oliver Brehm <brehm@mobile-software.ag>
2020-04-08 01:36:51 +0200

    added firestore to store dice state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+
+interface DiceState {
+  number: Number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,9 +14,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'OpenGameBoard';
 
-  currentNumber = 1;
+  diceStateDoc: AngularFirestoreDocument<DiceState>;
+  diceState: DiceState;
+
+  constructor(private firestore: AngularFirestore) {
+    this.diceState = {number: 0};
+  }
+
+  ngOnInit() {
+    this.diceStateDoc = this.firestore.doc('diceState/default');
+    this.diceStateDoc.valueChanges().subscribe( diceState => {
+      this.diceState = diceState
+    });
+  }
 
   rollDice() {
-    this.currentNumber = Math.floor(Math.random() * 6 + 1);
+    var number = Math.floor(Math.random() * 6 + 1);
+    this.diceStateDoc.update({number: number });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgKF_vVuRcXnDmePzXqHvJquHfx1xM_Us",
+  authDomain: "opengameboard.firebaseapp.com",
+  databaseURL: "https://opengameboard.firebaseio.com",
+  projectId: "opengameboard",
+  storageBucket: "opengameboard.appspot.com",
+  messagingSenderId: "465982726983",
+  appId: "1:465982726983:web:a0f8e8b43d6803f8826262",
+  measurementId: "G-H7D2TY6Q70"
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
     AppRoutingModule
   ],
   providers: [],
