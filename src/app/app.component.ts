69300893b6a5853ed952abec913763b04e5ce2cc
Oliver Brehm <brehm@mobile-software.ag>
2020-04-08 12:38:54 +0200

    added table, cup, nuber of dices, notes and players

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 
+interface Room {
+  notes: String;
+}
+
 interface DiceState {
-  number: Number
+  diceCount: number;
+  inCup: Array<Number>;
+  onTable: Array<Number>;
+  playerName: String;
+}
+
+interface Player {
+  name: String;
 }
 
 @Component({
@@ -14,22 +25,142 @@ interface DiceState {
 export class AppComponent {
   title = 'OpenGameBoard';
 
+  playerName = 'Anonymous';
+
+  roomDoc: AngularFirestoreDocument<Room>
+  room: Room;
+
+  playersCollection: AngularFirestoreCollection<Player>;
+  players: Array<Player>;
+  playerDoc: AngularFirestoreDocument<Player>;
+  player: Player;
+
   diceStateDoc: AngularFirestoreDocument<DiceState>;
   diceState: DiceState;
 
   constructor(private firestore: AngularFirestore) {
-    this.diceState = {number: 0};
+    this.diceState = {
+      diceCount: 0,
+      inCup: [],
+      onTable: [],
+      playerName: ""
+    };
+
+    this.room = {
+      notes: ""
+    }
   }
 
   ngOnInit() {
-    this.diceStateDoc = this.firestore.doc('diceState/default');
+    this.diceStateDoc = this.firestore.doc('rooms/mwD728EWLfKKgqbaiDpH/state/dice');
     this.diceStateDoc.valueChanges().subscribe( diceState => {
       this.diceState = diceState
     });
+
+    this.roomDoc = this.firestore.doc('rooms/mwD728EWLfKKgqbaiDpH');
+    this.roomDoc.valueChanges().subscribe( room => {
+      this.room = room;
+    });
+
+    this.playersCollection = this.firestore.collection('rooms/mwD728EWLfKKgqbaiDpH/players');
+    this.playersCollection.valueChanges().subscribe( players => {
+      this.players = players;
+    });
+
+    this.changePlayerName();
+
+    this.playersCollection.add({name: this.playerName}).then( docRef => {
+      this.playerDoc = this.firestore.doc(docRef);
+    });
   }
 
-  rollDice() {
-    var number = Math.floor(Math.random() * 6 + 1);
-    this.diceStateDoc.update({number: number });
+  @HostListener('window:beforeunload', [ '$event' ])
+  beforeunloadHandler(event) {
+    this.playerDoc.delete();
+  }
+
+  textareaChanged() {
+    this.roomDoc.update(this.room);
+  }
+
+  increaseDiceCount() {
+    this.diceState.diceCount++;
+    this.updateDices();
+  }
+
+  decreaseDiceCount() {
+    this.diceState.diceCount--;
+    this.updateDices();
+  }
+
+  updateDices() {
+    let inCup = [];
+    for(var i = 0; i < this.diceState.diceCount; i++) {
+      inCup.push(0);
+    }
+
+    this.diceState = {
+      diceCount: this.diceState.diceCount,
+      inCup: inCup,
+      onTable: [],
+      playerName: ""
+    }
+    this.update();
+  }
+
+  putInCup(dice: Number) {
+    let toRemove = this.diceState.onTable.indexOf(dice);
+
+    if(toRemove >= 0) {
+      this.diceState.onTable.splice(toRemove, 1);
+      this.diceState.inCup.push(dice);
+    }
+
+    this.update();
+  }
+
+  rollDicesInCup() {
+    if(this.diceState.inCup.length < 1) {
+      return;
+    }
+
+    this.diceState.playerName = this.playerName;
+
+    let inCupCount = this.diceState.inCup.length;
+    let onTable = this.diceState.onTable;
+
+    for(var i = 0; i < inCupCount; i++) {
+      onTable.push(this.rollDice());
+    }
+
+    this.diceState.onTable = onTable;
+    this.diceState.inCup = [];
+
+    this.update();
+  }
+
+  rollDice() : Number {
+    return Math.floor(Math.random() * 6 + 1);;
+  }
+
+  changePlayerName() {
+    let name = prompt("What's your name?");
+    if(name.length > 0) {
+      this.playerName = name;
+    } else {
+      this.playerName = "Anonymous"
+    }
+
+    if(this.playerDoc) {
+      this.playerDoc.update({name: name});
+    }
+  }
+
+  removePlayer(player: Player) {
+    // TODO remove player from db
+  }
+
+  update() {
+    this.diceStateDoc.update(this.diceState);
   }
 }
